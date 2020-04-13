5090a8ea135ff981731ac691dfd2d525e1a2dae2
Sandra Zhen <szhen8930@gmail.com>
2020-04-12 20:55:47 -0400

    further experiment

---
+++ assets/unused/experiment/maps/lab/lab_tiles/scifi_space_rpg_tile_set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="scifi_space_rpg_tiles" tilewidth="48" tileheight="48" tilecount="144" columns="12">
+ <image source="scifi_space_rpg_tiles.png" width="576" height="576"/>
+</tileset>
